0127acc38edee953037d36c2c207744c3366519c
hiroyuki.shirakawa <hiroyuki.shirakawa@globalsign.com>
2014-12-29 18:04:05 -0500

    Add a feature to set parent page

--- redmine_spaces.py
+++ redmine_spaces.py
@@ -1,3 +1,4 @@
 spaces = {  "test":"TEST" }
+parent_pages = { "test": "11111111" }  # set page id, can find id by https://confluence.atlassian.com/display/CONFKB/How+to+Get+Confluence+Page+ID+From+The+User+Interface
 
 jira_url="{jiraissues:url=https://your.jira.url/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=%22External+issue+ID%22+%7E+\g<1>&tempMax=10|columns=key,summary,status}"
