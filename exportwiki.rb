0fa74609ec6d25b039e93e2d080ed1c7746bb59b
Ivan Kanis <ivan.kanis@kanis.fr>
2015-12-22 12:20:35 +0100

    Improvements to the conversion scripts.
    - handle page with weird character title such as / or ?
    - shield (\) characters that will cause the export to fail
    - protect hyphen with unicode hyphen
    - process line by line to handle verbatim correctly
    - protect square bracket with double width bracket
    - clean content within textile attachment

--- exportwiki.rb
+++ exportwiki.rb
@@ -1,31 +1,44 @@
 class ExportWiki
 
-def self.export_text(p)
-   c = p.content_for_version(nil)
-   "#{c.text}"
-end
-def self.export_attach(dir, wikipage)
-	unless wikipage.attachments.empty? 
-		dir = dir + "/" +  wikipage.title
-		Dir.mkdir(dir)
-		wikipage.attachments.each { |a|FileUtils.cp(a.diskfile,  dir + "/" + a.filename ) }
-	end
-	true
+  def self.export_text(p)
+    c = p.content_for_version(nil)
+    "#{c.text}"
+  end
+
+  # extract first version author and date 
+  def self.export_extra_info(dir, p)
+    File.open(dir + "/" + p.title + ".extra-info", "w") do |f|
+      f.printf("'%s' '%s'\n", User.find_by_id(p.content.versions[0].author_id).login, p.content.versions[0].updated_on)
+    end
+  end
+
+  def self.export_attach(dir, wikipage)
+    unless wikipage.attachments.empty?
+      dir = dir + "/" +  wikipage.title
+      Dir.mkdir(dir)
+      wikipage.attachments.each { |a|FileUtils.cp(a.diskfile,  dir + "/" + a.filename ) }
+    end
+    true
+  end
+
+  def self.export_wiki(dir, wiki)
+    dir = dir + "/" + wiki.project.identifier
+    Dir.mkdir(dir)
+    wiki.pages.each do |p|
+      export_attach( dir, p )
+      export_extra_info( dir, p )
+      File.open(dir + "/" + p.title + ".textile", "w") { |f| f.write(export_text(p)) }
+    end
+    true
+  end
+
+
+  def self.export_all
+    system ("mkdir -p /tmp/redmine")
+    Project.all.each { |prj| export_wiki("/tmp/redmine/", prj.wiki) }
+  end
+
 end
 
-def self.export_wiki(dir, wiki)
-  dir = dir + "/" + wiki.project.identifier
-  Dir.mkdir(dir)
-  wiki.pages.each do |p|
-		export_attach( dir, p )
-		File.open(dir + "/" + p.title + ".textile", "w") { |f| f.write(export_text(p)) } 
-		end
-	true
-end
+# vim:et:sw=2:ts=2:
 
-
-def self.export_all
-	Project.all.each { |prj| export_wiki("/tmp/redmine/", prj.wiki) }
-end
-
-end
--- import_confluence.py
+++ import_confluence.py
@@ -7,7 +7,7 @@ from xmlrpclib import ServerProxy, Fault
 # redmine data processing functions
 import redmine
 # we need a list of redmine spaces for reference
-from redmine_spaces import spaces, parent_pages
+from redmine_spaces import spaces, parent_pages, replace_page
 
 # Connects to confluence server with username and password
 site_URL = "https://your.confluence.site/"
@@ -38,9 +38,6 @@ def save_attachments(space, filename, curpage):
                 # I have no idea which exception this thing throws, so all of them.
                 print "ERROR: Can not upload file '{0}' for the page '{1}' in space {2}".format(filename[:-8]+'/' + attachmentfile, curpage['title'], space)
 
-
-
-
 def save(space, filename, pagename, project_name):
     # Retrives text from a file and converts it
     content = redmine.wiki_to_confluence(filename)
@@ -54,8 +51,6 @@ def save(space, filename, pagename, project_name):
     curpage = server.confluence2.getPage(token,space,pagename)
     save_attachments(space, filename, curpage)
 
-
-
 for project_dir, subdirs, files in os.walk('.'):
     try:
         project_name = project_dir.split('/')[1]
@@ -67,6 +62,9 @@ for project_dir, subdirs, files in os.walk('.'):
             # If page name is 'Wiki' it needs to be changed in order to avoid conflicts
             if pagename == 'Wiki':
                 pagename = project_name + ' Wiki'
+            # Fix page that had weird character such as / or ?.
+            if pagename in replace_page:
+                pagename = replace_page[pagename]
         else:
             continue
         try:
@@ -79,7 +77,9 @@ for project_dir, subdirs, files in os.walk('.'):
         else:
             print "Page %s exists" % pagename
             continue
-        print "Saving page %s"% pagename
+        print "Saving page %s" % pagename
         save(spaces[project_name], os.path.join(project_dir,filename),pagename, project_name)
 
 server.confluence2.logout(token)
+
+# vim:et:sw=4:ts=4:
